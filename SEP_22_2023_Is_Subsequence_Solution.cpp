ca82e97fb57e5963cfb2680f42cf3af851801bdb
Avinash Agrawal <avinash03082003@gmail.com>
2023-09-22 22:42:20 +0530

    Added Sep 22 POTD

---
+++ SEP_22_2023_Is_Subsequence_Solution.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0, j = 0, n = s.size(), m = t.size();
+        while(i<n && j<m){
+            if(s[i] == t[j]) i++;
+            j++;
+        }
+        return i==n;
+    }
+};
