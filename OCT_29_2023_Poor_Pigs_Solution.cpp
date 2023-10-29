7d9389098be5f5a4e41fb5155c44af52e15b65ec
Avinash Agrawal <avinash03082003@gmail.com>
2023-10-29 12:10:34 +0530

    Added Oct 29 POTD

---
+++ OCT_29_2023_Poor_Pigs_Solution.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int poorPigs(int buckets, int timeDetect, int timeTest) {
+        return ceil(log2(buckets)/log2(int(timeTest/timeDetect)+1));
+    }
+};
