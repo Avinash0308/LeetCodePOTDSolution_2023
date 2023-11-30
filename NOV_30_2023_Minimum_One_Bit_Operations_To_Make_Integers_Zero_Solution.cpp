f56bd7a7b6f3810d7e4b7085b67e47a88451b7ae
Avinash Agrawal <avinash03082003@gmail.com>
2023-11-30 21:04:25 +0530

    Added Nov 30 POTD

---
+++ NOV_30_2023_Minimum_One_Bit_Operations_To_Make_Integers_Zero_Solution.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        int res;
+        for (res = 0; n > 0; n &= n - 1)
+            res = -(res + (n ^ (n - 1)));
+        return abs(res);
+    }
+};
