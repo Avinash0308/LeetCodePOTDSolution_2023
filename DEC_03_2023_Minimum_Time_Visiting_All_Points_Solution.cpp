356cf2662596c5636e730c0f54df05ed2549bc71
Avinash Agrawal <avinash03082003@gmail.com>
2023-12-03 11:00:14 +0530

    Solved Dec 03 POTD

---
+++ DEC_03_2023_Minimum_Time_Visiting_All_Points_Solution.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int ans = 0;
+        int n = points.size();
+        for(int i = 1; i<n; i++){
+            int x = abs(points[i][0] - points[i-1][0]);
+            int y = abs(points[i][1] - points[i-1][1]);
+            ans += max(x,y);
+        }
+        return ans;
+    }
+};
