100cf7e2280bfbc2d0b8a24f6629a8a91c21e3be
Avinash Agrawal <avinash03082003@gmail.com>
2023-12-21 21:42:50 +0530

    Added Dec 21 POTD

---
+++ DEC_21_2023_Widest_Vertical_Area_Between_Two_Points_Containing_No_Points_Solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int maxWidthOfVerticalArea(vector<vector<int>> &points)
+    {
+        sort(points.begin(), points.end());
+        int ans = 0;
+        for (int i = 1; i < points.size(); i++)
+            ans = max(ans, points[i][0] - points[i - 1][0]);
+        return ans;
+    }
+};
